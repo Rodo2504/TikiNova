9230aa2d9f7c174f6f7f8aad91ab7a529cdf6068
alphasebas <alphasebas117@hotmail.com>
2020-06-17 18:33:46 -0500

    Prueba Screen Reader

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-
+import Speech from 'speak-tts';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
+  html = '';
+  speech: any;
+  speechData: any;
+  constructor(){
 
-  ngOnInit(): void {
+    this.speech = new Speech() // will throw an exception if not browser supported
+    if(this.speech .hasBrowserSupport()) { // returns a boolean
+        console.log("speech synthesis supported")
+        this.speech.init({
+                'volume': 1,
+                'lang': 'es-MX',
+                'rate': 1,
+                'pitch': 1,
+                'voice':'Microsoft Sabina Desktop - Spanish (Mexico)',
+                'splitSentences': true,
+                'listeners': {
+                    'onvoiceschanged': (voices) => {
+                        console.log("Event voiceschanged", voices)
+                    }
+                }
+        }).then((data) => {
+            // The "data" object contains the list of available voices and the voice synthesis params
+            console.log("Speech is ready, voices are available", data)
+            this.speechData = data;
+            data.voices.forEach(voice => {
+            console.log(voice.name + " "+ voice.lang)
+            });
+        }).catch(e => {
+            console.error("An error occured while initializing : ", e)
+        })
+    }
   }
 
+  start(texto:string){
+
+    this.html = texto;
+    console.log(texto);
+
+
+      this.speech.speak({
+          text: this.html,
+      }).then(() => {
+          console.log("Success !")
+      }).catch(e => {
+          console.error("An error occurred :", e)
+      })
+  }
+  pause(){
+    this.speech.pause();
+  }
+  resume(){
+    this.speech.resume();
+  }
+
+
+
 }
+
+
+
