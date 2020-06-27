41fd6eab1fdbcb5c4cfbfc120fd4490f1a44d66f
Rubennova <ruben-marquezr@hotmail.com>
2020-06-27 00:32:33 -0500

    Recibir a angular las consultas

--- src/app/components/cliente/pedido/qr/qr.component.ts
+++ src/app/components/cliente/pedido/qr/qr.component.ts
@@ -5,14 +5,14 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./qr.component.css']
 })
 export class QRComponent {
-@Input() codstr:string;
-@Input() codval:number;
+// @Input() codstr:string;
+// @Input() codval:number;
   public qrdata: any = null;
   public elementType: "img" | "url" | "canvas" | "svg" = null;
   public scale: number = 1;
 
   constructor() {
-    this.qrdata = "CODIGO DE DESCUENTO: "+this.codval+"DESC"+this.codstr;
+    // this.qrdata = "CODIGO DE DESCUENTO: "+this.codval+"DESC"+this.codstr;
     this.elementType = "img";
     this.scale = 4;
    }
